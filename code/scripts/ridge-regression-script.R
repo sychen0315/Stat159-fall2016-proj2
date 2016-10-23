fab9303bd862b55399bd89ee0c0071b7f8ab677c
sychen0315 <sychen0315@gmail.com>
2016-10-23 00:43:26 -0700

    ridge: modifies ridge-script.R

--- code/scripts/ridge-regression-script.R
+++ code/scripts/ridge-regression-script.R
@@ -1,5 +1,6 @@
 # Ridge Regression Models
 library(Matrix)
+library(foreach)
 library(glmnet)
 
 # Read Data
@@ -7,44 +8,44 @@ scaled_credit <- read.csv("../../data/scaled-credit.csv", sep = ",", row.names =
 training_set <- read.csv("../../data/training-set.csv", sep = ",", row.names = 1)
 testing_set <- read.csv("../../data/testing_set.csv", sep = ",", row.names = 1)
 
-x <- data.matrix(scaled_credit[,-1])
+x <- data.matrix(scaled_credit[,1:11])
 y <- scaled_credit$Balance
 
-x_trainning <- data.matrix(training_set[,-1])
+x_trainning <- data.matrix(training_set[,1:11])
 y_trainning <- training_set$Balance
 
-x_testing <- data.matrix(testing_set[,-1])
+x_testing <- data.matrix(testing_set[,1:11])
 y_testing <- testing_set$Balance
 
 # Cross validation
 set.seed(1)
-ridge_cv_out <- cv.glmnet(x_trainning, y_tainning, alpha=0, intercept = FALSE,
+ridge_cv_out <- cv.glmnet(x_trainning, y_trainning, alpha=0, intercept = FALSE,
                     standardize = FALSE)
 
 
 # Select the best model
-bestlam <- ridge_cv_out$lambda.min
+ridge_bestlam <- ridge_cv_out$lambda.min
 
 # Plot the cross-validation errors
-png(file = "../../images/ridge-images/cv-errors.png")
+png(file = "../../images/ridge-images/ridge-cv-errors.png")
 plot(ridge_cv_out)
 dev.off()
 
 # Compute MSE using test set
-prediction1 <- predict(ridge_cv_out, s = bestlam, newx = x_testing)
-ridge_MSE <- mean((prediction1- y_testing)^2)
+ridge_pred <- predict(ridge_cv_out, s = ridge_bestlam, newx = x_testing)
+ridge_MSE <- mean((ridge_pred - y_testing)^2)
 
 # Refit the model
 out <- glmnet(x, y, alpha = 0, intercept = FALSE, standardize = FALSE)
-ridge_coef <- predict(out, s = bestlam, type="coefficients")[1:11,]
+ridge_coef <- predict(out, s = ridge_bestlam, type="coefficients")[1:12,]
 
 # Save output
-save(cv_out, bestlam, ridge_MSE, ridge_coef, file = "../../data/ridge-regression.RData")
+save(ridge_cv_out, ridge_bestlam, ridge_MSE, ridge_coef, file = "../../data/ridge-regression.RData")
 
-sink(file = "../../data/regression-output.txt")
+sink(file = "../../data/ridge-output.txt")
 writeLines("Ridge Regression")
 writeLines("Best Lambda")
-bestlam
+ridge_bestlam
 writeLines("Official coefficients")
 ridge_coef
 writeLines("Test MSE")
