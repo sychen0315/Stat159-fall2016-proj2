c768ca81255c0d23a7b1567331d119360f0d4d3b
sychen0315 <sychen0315@gmail.com>
2016-10-23 11:18:22 -0700

    sessionInfo: create sessionInfo script

---
+++ code/scripts/session-info-script.R
@@ -0,0 +1,18 @@
+# Print session information
+library(devtools)
+library(stats)
+library(rstudioapi)
+library(Matrix)
+library(foreach)
+library(glmnet)
+library(pls)
+library(rmarkdown)
+library(xtable)
+library(ggplot2)
+
+
+sink("../../session-info.txt")
+cat("Session Information\n\n")
+print(sessionInfo())
+devtools::session_info(s)
+sink()
