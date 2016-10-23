f3876828790c6099e387c71164946ded417a29b6
sychen0315 <sychen0315@gmail.com>
2016-10-22 22:02:02 -0700

    eda: create a sub-directory under images directory to store all eda plots

--- code/scripts/eda-script.R
+++ code/scripts/eda-script.R
@@ -18,14 +18,14 @@ for (i in c(seq(1,6), 11)) {
   }
   # Plot histograms
   hist_title <- paste("Histogram of",colnames(credit_df)[i]) 
-  hist_path <- "../../images/histogram-"
+  hist_path <- "../../images/eda-images/histogram-"
   hist_filename <- paste(hist_path, tolower(colnames(credit_df)[i]), 
                          ".png", sep = "")
   eda_hist(credit_df[[i]], hist_title, hist_filename)
   
   # Plot boxplots
   box_title <- paste("Boxplot of",colnames(credit_df)[i]) 
-  box_path <- "../../images/boxplot-"
+  box_path <- "../../images/eda-images/boxplot-"
   box_filename <- paste(box_path, tolower(colnames(credit_df)[i]), 
                         ".png", sep = "")
   eda_boxplot(credit_df[[i]], box_title, box_filename)
@@ -47,10 +47,10 @@ married_freq <- eda_freq(credit_df$Married)
 ethnicity_freq <- eda_freq(credit_df$Ethnicity)
 
 # Make plots for qualitative variables
-eda_barplot(credit_df$Gender, "Barplot of Gender", "../../images/barplot-gender.png")
-eda_barplot(credit_df$Student, "Barplot of Student", "../../images/barplot-student.png")
-eda_barplot(credit_df$Married, "Barplot of Married", "../../images/barplot-married.png")
-eda_barplot(credit_df$Ethnicity, "Barplot of Ethnicity", "../../images/barplot-ethnicity.png")
+eda_barplot(credit_df$Gender, "Barplot of Gender", "../../images/eda-images/barplot-gender.png")
+eda_barplot(credit_df$Student, "Barplot of Student", "../../images/eda-images/barplot-student.png")
+eda_barplot(credit_df$Married, "Barplot of Married", "../../images/eda-images/barplot-married.png")
+eda_barplot(credit_df$Ethnicity, "Barplot of Ethnicity", "../../images/eda-images/barplot-ethnicity.png")
 
 # Save frequency results
 sink(file = "../../data/eda-qualitative-freq.txt")
@@ -82,7 +82,7 @@ sink()
 save(matrix_cor, file = "../../data/correlation-matrix.RData")
 
 # Scatterplot matrix
-png("../../images/scatterplot-matrix.png")
+png("../../images/eda-images/scatterplot-matrix.png")
 pairs(~Income+Limit+Rating+Cards+Age+Education+Balance, data = credit_df, 
       main = 'Simple Scatterplot Matrix', cex = 0.8)
 dev.off()
@@ -101,7 +101,7 @@ save(aov_qualitative, file = "../../data/eda-qualitative-anova.RData")
 # On gender
 balance_male = credit_df$Balance[credit_df$Gender == ' Male']
 balance_female = credit_df$Balance[credit_df$Gender == 'Female']
-png('../../images/boxplot-Balance-conditional-Gender.png')
+png('../../images/eda-images/boxplot-Balance-conditional-Gender.png')
 boxplot(balance_male, balance_female, names = c('Male', 'Female'), 
         main = 'Boxplot of Balance Conditioned on Gender')
 dev.off()
@@ -109,7 +109,7 @@ dev.off()
 # On student
 balance_student = credit_df$Balance[credit_df$Student == 'Yes']
 balance_nonstudent = credit_df$Balance[credit_df$Student == 'No']
-png('../../images/boxplot-Balance-conditional-Student.png')
+png('../../images/eda-images/boxplot-Balance-conditional-Student.png')
 boxplot(balance_student, balance_nonstudent, names = c('Student', 'Non-Student'), 
         main = 'Boxplot of Balance Conditioned on Student')
 dev.off()
@@ -117,7 +117,7 @@ dev.off()
 # On marital status
 balance_married = credit_df$Balance[credit_df$Married == 'Yes']
 balance_single = credit_df$Balance[credit_df$Married == 'No']
-png('../../images/boxplot-Balance-conditional-Married.png')
+png('../../images/eda-images/boxplot-Balance-conditional-Married.png')
 boxplot(balance_married, balance_single, names = c('Married', 'Single'),
         main = 'Boxplot of Balance Conditioned on Marital Status')
 dev.off()
@@ -126,7 +126,7 @@ dev.off()
 balance_Caucasian = credit_df$Balance[credit_df$Ethnicity == 'Caucasian']
 balance_AA = credit_df$Balance[credit_df$Ethnicity == 'African American']
 balance_Asian = credit_df$Balance[credit_df$Ethnicity == 'Asian']
-png('../../images/boxplot-Balance-conditional-Ethnicity.png')
+png('../../images/eda-images/boxplot-Balance-conditional-Ethnicity.png')
 boxplot(balance_Caucasian, balance_AA, balance_Asian, 
         names = c('Caucasian', 'African American', 'Asian'), 
         main = 'Boxplot of Balance Conditioned on Ethnicity')
